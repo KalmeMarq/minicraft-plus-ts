ebbfc09396f1e0fbaf2a2e41c28773ad05bedd41
KalmeMarq <marq520filip56.fm@gmail.com>
2022-06-01 01:08:21 +0100

    updated font and removed snbt

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
 import { Identifier } from '@src/Identifier';
-
-console.log(new Identifier('apple').toString());
-console.log(new Identifier('minicraft:wood').toString());
-console.log(new Identifier('km:bread').toString());
